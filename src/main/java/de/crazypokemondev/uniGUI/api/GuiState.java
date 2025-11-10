b00bed7c841eeaecedbb8fd45a6fc581131e6dd1
Olfi01 <flom@meyer-alpers.de>
2025-11-10 18:17:30 +0100

    fix: avoid ConcurrentModificationException

--- src/main/java/de/crazypokemondev/uniGUI/api/GuiState.java
+++ src/main/java/de/crazypokemondev/uniGUI/api/GuiState.java
@@ -1,10 +1,10 @@
 package de.crazypokemondev.uniGUI.api;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface GuiState {
-    List<Runnable> updateHandlers = new ArrayList<>();
+    List<Runnable> updateHandlers = new CopyOnWriteArrayList<>();
 
     default void registerUpdateHandler(Runnable updateHandler) {
         updateHandlers.add(updateHandler);
